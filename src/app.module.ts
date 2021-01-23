376bde4be31f53c84943744d0173d90d638a26c9
Vimal Tagline <vimal.tagline@gmail.com>
2021-01-23 13:55:30 +0530

    path changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { UserModule } from '@api/user/user.module';
-import { AuthModule } from 'api/auth/auth.module';
-import { ConfigService } from '@config/services/config.service';
-import { ConfigModule } from '@config/config.module'
-import { SharedModule } from '@shared/shared.module';
-import { GiftModule } from '@api/admin/gift/gift.module';
-import { OccasionModule } from '@api/admin/occasion/occasion.module';
-import { CountryModule } from '@api/admin/country/country.module';
-import { UserReminderModule } from '@api/user/userReminder/userReminder.module';
+import { UserModule } from './api/user/user.module';
+import { AuthModule } from './api/auth/auth.module';
+import { ConfigService } from './config/services/config.service';
+import { ConfigModule } from './config/config.module'
+import { SharedModule } from './shared/shared.module';
+import { GiftModule } from './api/admin/gift/gift.module';
+import { OccasionModule } from './api/admin/occasion/occasion.module';
+import { CountryModule } from './api/admin/country/country.module';
+import { UserReminderModule } from './api/user/userReminder/userReminder.module';
 
 @Module({
   imports: [
--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,19 +1,19 @@
 import { Module, Global } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
-import { AuthModule } from '@api/auth/auth.module';
-import { UsersSchema } from '@models/user.model';
-import { GiftSchema } from '@models/gift.model';
-import { CountrySchema } from '@models/country.model';
-import { OccasionSchema } from '@models/occasion.model';
-import { UserService } from '@api/user/user.service';
-import { AuthService } from '@api/auth/auth.service'
+import { AuthModule } from '../api/auth/auth.module';
+import { UsersSchema } from '../models/user.model';
+import { GiftSchema } from '../models/gift.model';
+import { CountrySchema } from '../models/country.model';
+import { OccasionSchema } from '../models/occasion.model';
+import { UserService } from '../api/user/user.service';
+import { AuthService } from '../api/auth/auth.service'
 import { CommonService } from './services/common.service';
-import { CountryService } from '@api/admin/country/country.service';
-import { GiftService } from '@api/admin/gift/gift.service';
-import { OccasionService } from '@api/admin/occasion/occasion.service';
-import { UsersRemindersSchema } from '@models/userReminders.model';
-import { UserReminderService } from '@root/api/user/userReminder/userReminder.service';
+import { CountryService } from '../api/admin/country/country.service';
+import { GiftService } from '../api/admin/gift/gift.service';
+import { OccasionService } from '../api/admin/occasion/occasion.service';
+import { UsersRemindersSchema } from '../models/userReminders.model';
+import { UserReminderService } from '../api/user/userReminder/userReminder.service';
 
 @Global()
 @Module({
